c313b1e16566ae0d31c6fcbe48b6264876cb15ed
Kieran <kicu1811@gmail.com>
2024-01-26 09:00:19 +0100

    la til spiller2

--- spill.py
+++ spill.py
@@ -8,13 +8,13 @@ class Spillobjekt:
         self.y = start_y
         self.color = "grey"
         self.size = 5
-        self.rect = pygame.Rect((self.x - self.size, self.y - self.size), (self.size * 2, self.sie * 2))
+        self.rect = pygame.Rect((self.x - self.size, self.y - self.size), (self.size * 2, self.size * 2))
     
     def tegn(self):
         pygame.draw.rect(screen, self.color, self.rect)
-        self.rect = pygame.Rect((self.x - self.size, self.y - self.size), (self.size * 2, self.sie * 2))
+        self.rect = pygame.Rect((self.x - self.size, self.y - self.size), (self.size * 2, self.size * 2))
 
-class Spiller(Spillobjekt):
+class Spiller1(Spillobjekt):
     def __init__(self, start_x, start_y):
         super().__init__(start_x, start_y)
         self.fart = 5
@@ -33,15 +33,37 @@ class Spiller(Spillobjekt):
             self.x += self.fart
         if keys[pygame.K_w]:
             self.y -= self.fart
+        if keys[pygame.K_s]:
+            self.y += self.fart
 
 class Spiller2(Spillobjekt):
     def __init__(self, start_x, start_y):
         super().__init__(start_x, start_y)
         self.fart = 5
+        self.size = 5
+        self.color = "blue"
+    
+    def tegn(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+        self.rect = pygame.Rect((self.x - self.size, self.y - self.size), (self.size * 3, self.size * 3))
+
+    def oppdater(self):
+        keys = pygame.key.get_pressed() 
+        if keys[pygame.K_LEFT]:
+            self.x -= self.fart
+        if keys[pygame.K_RIGHT]:
+            self.x += self.fart
+        if keys[pygame.K_UP]:
+            self.y -= self.fart
+        if keys[pygame.K_DOWN]:
+            self.y += self.fart
 
 screen = pygame.display.set_mode((500, 500)) # Setter skjermen til 500x500 piksler.
 
 clock = pygame.time.Clock()
+
+spiller1 = Spiller1(screen.get_width()/2, screen.get_height()/2)
+spiller2 = Spiller2(screen.get_width()/2, screen.get_height()/2)
 
 running = True
 
@@ -58,9 +80,11 @@ while running:
     Her skal vi putte spillets logikk, som å tegne figurer og oppdatere posisjonen deres.
     """
 
-    Spiller.tegn()
-    Spiller.oppdater()
+    spiller1.tegn()
+    spiller1.oppdater()
 
+    spiller2.tegn()
+    spiller2.oppdater()
 
     # Oppdaterer hele skjermen
     pygame.display.flip()
